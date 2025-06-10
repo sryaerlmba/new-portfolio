1aadad636676e1e3120935e0ad87f96fd3925c0d
sryaerlmba <suryaerlamba3@gmail.com>
2025-06-10 07:48:48 +0700

    delete Nprogress in vite

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['nprogress']
-    }
-  }
 })
